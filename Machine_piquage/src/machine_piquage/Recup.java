5d35f7b631d4b3df5b6232057fb3ea669d8121cd
mohamed <m.mokadem202@hotmail.fr>
2017-04-14 11:56:44 +0200

    dossier machine de piquage

---
+++ Machine_piquage/src/machine_piquage/Recup.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package machine_piquage;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+/* This class check is user don't enter anything */
+/**
+ *
+ * @author Mohamed
+ */
+public class Recup {
+    
+
+
+/*
+public void isOnlyNumber(String value) {
+
+    try{
+          Integer.parseInt(value);
+    }
+    catch (NumberFormatException nfe) {
+        Component frame = null;
+        JOptionPane.showConfirmDialog(null,
+                "MESSAGE",
+                "TITLE",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE);
+  }
+return true;
+}
+
+}
+*/
+    
+}
